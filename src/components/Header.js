6196369bc3d0173794dd801d094c2cf845183966
asarenski <asarenski@gmail.com>
2021-03-14 11:52:38 -0400

    added Header and Proposal section

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='Header'>
+            <h1>Coming Soon!</h1>
+            <h5>In 2022 or 2023...</h5>
+            <p>The wedding of Mr. Adam Sarenski and Dr. Ann Marie Seidel.</p>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/TheProposal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TheProposal = () => {
+    return (
+        <section className='TheProposal'>
+            <h2>The Proposal</h2>
+            <p>Lorem ipsum....</p>
+        </section>
+    );
+};
+
+export default TheProposal;
