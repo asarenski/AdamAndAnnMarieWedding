c54eb260b623bc632e879b3b123aead8c5d18638
asarenski <asarenski@gmail.com>
2021-04-17 10:16:42 -0400

    trying another way to force https

--- src/server/server.js
+++ src/server/server.js
@@ -3,21 +3,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const requireHTTPS = require('./requireHTTPS');
 const routes = require('./routes');
+
 app.use(express.static(path.join(__dirname,'../../build')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-app.use(express.static('dist'));
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://adamannmariewedding.ue.r.appspot.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Strict-Transport-Security', 'max-age=31536000');
     next();
 });
 
+app.use(requireHTTPS);
 
 app.get('/api/checkHealth', routes.checkHealth);
 app.get('/*', (req, res) => {
