d7023d0441e0f5a7ab0fd8bf80c1035eaf35cbf6
asarenski <asarenski@gmail.com>
2021-04-17 09:53:05 -0400

    set build to gcp-build

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const routes = require('./routes');
-app.use(express.static(path.join(__dirname,"../../build")));
+app.use(express.static(path.join(__dirname,'../../build')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
@@ -12,8 +12,9 @@ app.use(bodyParser.json())
 app.use(express.static('dist'));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://adamannmariewedding.ue.r.appspot.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', 'https://adamannmariewedding.ue.r.appspot.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Strict-Transport-Security', 'max-age=31536000');
     next();
 });
 
